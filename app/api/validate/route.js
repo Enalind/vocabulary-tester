90824eff41ba447b43483a637988e830a8cc0bfb
Elias Lindström <enalindstrom@gmail.com>
2023-06-15 18:14:10 +0200

    Change ngrok URL

--- app/api/validate/route.js
+++ app/api/validate/route.js
@@ -1,7 +1,7 @@
 
 export async function POST(word, target, langauge){
 
-    const response = await fetch("https://f6c7-94-255-188-31.ngrok-free.app/glossary?" + new URLSearchParams({
+    const response = await fetch("https://5c7b-94-255-188-31.ngrok-free.app/glossary?" + new URLSearchParams({
         targetWord: target.toLowerCase(),
         word: word.toLowerCase(),
         la: langauge
